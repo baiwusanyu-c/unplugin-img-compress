7b3ba3296bcafa5b3db08ccc6f16507a2a6da7fa
baiwusanyu-c <chenzhihui@chabaidao.com>
2024-11-06 16:37:36 +0800

    feat: added read_file rs

--- packages/unplugin-img-compress-fs/src/lib.rs
+++ packages/unplugin-img-compress-fs/src/lib.rs
@@ -1,11 +1,21 @@
 #![deny(clippy::all)]
-use std::fs::{ canonicalize, read };
+use std::fs::{ read, write};
+use path_clean::{clean};
 #[macro_use]
 extern crate napi_derive;
 
 #[napi]
 pub fn read_file(path: String) -> Result<Vec<u8>, napi::Error> {
-  let normalize_path = canonicalize(path).map_err(|e| napi::Error::new(napi::Status::GenericFailure, format!("路径标准化失败: {}", e)))?;
-  let data = read(normalize_path).map_err(|e| napi::Error::new(napi::Status::GenericFailure, format!("读取文件失败: {}", e)))?;
+  let normalize_path = clean(path);
+  let data = read(normalize_path)
+    .map_err(|e| napi::Error::new(napi::Status::GenericFailure, format!("读取文件失败: {}", e)))?;
   Ok(data)
+}
+
+#[napi]
+pub fn out_put_file(path: String, data: Vec<u8>) -> Result<(), napi::Error> {
+  let normalize_path = clean(path);
+   write(normalize_path, data)
+      .map_err(|e| napi::Error::new(napi::Status::GenericFailure, format!("存储文件失败: {}", e)))?;
+  Ok(())
 }
---
+++ play/vite/test.mjs
@@ -0,0 +1,32 @@
+import { outPutFile, readFile } from 'unplugin-img-compress-fs'
+import tinify from 'tinify'
+
+export const tinifyBuffer = (data, APIKey) => {
+  tinify.key = APIKey
+  return new Promise((resolve) => {
+    tinify.fromBuffer(data)
+      .toBuffer((err, resultData) => {
+        if (err)
+          throw err
+
+        resolve(resultData)
+      })
+  })
+}
+
+async function start() {
+  const data = Buffer.from(readFile('./home.png'))
+  const tinifyRes = await tinifyBuffer(data, 'kZgn8pxfdjQjKFmf2StLq7CY4TqMgs0T')
+  outPutFile('./homes.png', tinifyRes.toJSON().data)
+}
+start()
+// TODO: outputFile √
+// TODO: pathExists
+// TODO: readFile √
+// TODO: remove
+
+// TODO: readdir
+// TODO: readJson
+// TODO: writeJson
+// TODO: copy
+// TODO: stat
