ef5e94b1c2f88ed74e789c660d7da4009bb2f28e
白雾三语 <32354856+baiwusanyu-c@users.noreply.github.com>
2024-12-03 12:02:08 +0800

    feat: support rspack (#104)
    Co-authored-by: baiwusanyu-c <chenzhihui@chabaidao.com>

--- packages/unplugin-img-compress/src/runtime/build.ts
+++ packages/unplugin-img-compress/src/runtime/build.ts
@@ -2,9 +2,34 @@ import { log, setGlobalPrefix } from 'baiwusanyu-utils'
 import { createUnplugin } from 'unplugin'
 import { initOption } from '../../utils/option'
 import type { CompressOption, IBundle, writeBundle } from '../../utils/types'
-import type { UnpluginOptions } from 'unplugin'
+import type { RspackCompiler, UnpluginOptions } from 'unplugin'
 import type { PluginOption } from 'vite'
 import type { OutputOptions } from 'rollup'
+import type { Compilation as RspackCompilation } from '@rspack/core'
+import type { Compilation as WebpackCompilation, Compiler as WebpackCompiler } from 'webpack'
+function webRspackHandler(
+  ctx: { dir: string, bundle: IBundle },
+  compiler: RspackCompiler | WebpackCompiler,
+) {
+  compiler.hooks.emit.tapAsync('unplugin-img-compress', (
+    compilation: RspackCompilation | WebpackCompilation,
+    callback) => {
+    ctx.dir = (compilation.outputOptions.path || './dist').replaceAll('\\', '/')
+    for (const filename in compilation.assets) {
+      if (/\.(png|jpg|jpeg|gif|svg)$/.test(filename)) {
+        const bundleOrg = compilation.assets[filename]
+        ctx.bundle[filename] = {
+          source: bundleOrg.buffer(),
+          fileName: filename,
+          type: 'asset',
+          isAsset: true,
+          name: undefined,
+        }
+      }
+    }
+    callback()
+  })
+}
 
 const unplugin = createUnplugin(
   (option: CompressOption, meta) => {
@@ -15,7 +40,7 @@ const unplugin = createUnplugin(
         name: 'unplugin-img-compress',
       }
     }
-    const ctxWebpack: { dir: string, bundle: IBundle } = { dir: '', bundle: {} }
+    const ctxWebRspack: { dir: string, bundle: IBundle } = { dir: '', bundle: {} }
     return {
       name: 'unplugin-img-compress',
       writeBundle: async(
@@ -25,33 +50,21 @@ const unplugin = createUnplugin(
         log('info', '✨ running...')
         log('info', '✨ 【runtime dev】')
         if (optionInner.runtime === 'build' && optionInner.compressImgBundle) {
-          if (meta.framework !== 'webpack') {
+          if (meta.framework !== 'webpack' && meta.framework !== 'rspack') {
             const outputDir = outputOptions.dir!.replaceAll('\\', '/')
             optionInner.compressImgBundle(outputDir, optionInner.APIKey, bundle)
-          } else {
-            optionInner.compressImgBundle(ctxWebpack.dir, optionInner.APIKey, ctxWebpack.bundle)
+          } else if (meta.framework === 'webpack' || meta.framework === 'rspack') {
+            optionInner.compressImgBundle(ctxWebRspack.dir, optionInner.APIKey, ctxWebRspack.bundle)
           }
         }
       },
+      rspack(compiler: RspackCompiler) {
+        if (optionInner.runtime === 'build' && optionInner.compressImgBundle && meta.framework === 'rspack')
+          webRspackHandler(ctxWebRspack, compiler)
+      },
       webpack(compiler) {
-        if (optionInner.runtime === 'build' && optionInner.compressImgBundle && meta.framework === 'webpack') {
-          compiler.hooks.emit.tapAsync('ImageAssetsPlugin', (compilation, callback) => {
-            ctxWebpack.dir = (compilation.outputOptions.path || './dist').replaceAll('\\', '/')
-            for (const filename in compilation.assets) {
-              if (/\.(png|jpg|jpeg|gif|svg)$/.test(filename)) {
-                const bundleOrg = compilation.assets[filename]
-                ctxWebpack.bundle[filename] = {
-                  source: bundleOrg.buffer(),
-                  fileName: filename,
-                  type: 'asset',
-                  isAsset: true,
-                  name: undefined,
-                }
-              }
-            }
-            callback()
-          })
-        }
+        if (optionInner.runtime === 'build' && optionInner.compressImgBundle && meta.framework === 'webpack')
+          webRspackHandler(ctxWebRspack, compiler)
       },
     } as UnpluginOptions & { writeBundle?: writeBundle }
   })
@@ -60,3 +73,4 @@ export const viteImgCompress: (option: CompressOption) => PluginOption = unplugi
 export const rollupImgCompress = unplugin.rollup
 export const webpackImgCompress = unplugin.webpack
 export const esbuildImgCompress = unplugin.esbuild
+export const rspackImgCompress = unplugin.rspack
